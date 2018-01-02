fbe175f46396dc46eb5d7bf1c628ab548329834b
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-02 18:11:45 +0700

    detail job

--- src/screens/DetailJobScreen.js
+++ src/screens/DetailJobScreen.js
@@ -1,7 +1,19 @@
 import React from "react"
 import { View, Text } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
 
 class DetailJobScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: "DetailJob",
+    headerRight: (
+      <Ionicons
+        name="ios-bookmark-outline"
+        onPress={() => { }}
+        size={32}
+        style={{ marginRight: 10 }}
+      />
+    )
+  })
   render() {
     return (
       <View>
