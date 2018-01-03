1b9bb6b0cc45229f56969f45928ab11d1ddbfa6c
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-03 13:21:34 +0700

    add icons tab

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { TabNavigator, StackNavigator } from "react-navigation"
 
 import AuthScreen from "./screens/AuthScreen"
@@ -6,6 +7,7 @@ import MyJobsScreen from "./screens/MyJobsScreen"
 import SavedJobsScreen from "./screens/SavedJobsScreen"
 import DetailJobScreen from "./screens/DetailJobScreen"
 import NewJobScreen from "./screens/NewJobScreen"
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Routes = TabNavigator({
   Auth: {
@@ -23,17 +25,31 @@ const Routes = TabNavigator({
       Dashboard: {
         screen: StackNavigator({
           Dashboard: {
-            screen: DashboardScreen
+            screen: DashboardScreen,
           },
           DetailJob: {
             screen: DetailJobScreen,
           },
         }),
+        navigationOptions: {
+          tabBarIcon({ focused }) {
+            const name = focused ? "ios-home" : "ios-home-outline"
+            return <Ionicons name={name} size={32} />
+          },
+          tabBarLabel: "Dashboard",
+        },
       },
       SavedJobs: {
         screen: StackNavigator({
           SavedJobs: {
             screen: SavedJobsScreen,
+            navigationOptions: {
+              tabBarIcon({ focused }) {
+                const name = focused ? "ios-bookmark" : "ios-bookmark-outline"
+                return <Ionicons name={name} size={32} />
+              },
+              tabBarLabel: "Saved Jobs",
+            },
           },
         }),
       },
@@ -46,6 +62,13 @@ const Routes = TabNavigator({
             screen: NewJobScreen,
           },
         }),
+        navigationOptions: {
+          tabBarIcon({ focused }) {
+            const name = focused ? "ios-list" : "ios-list-outline"
+            return <Ionicons name={name} size={32} />
+          },
+          tabBarLabel: "My Jobs",
+        }
       },
     }),
     navigationOptions: {
