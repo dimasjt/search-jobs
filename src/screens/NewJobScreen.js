e11ca53d8efb9e943845143522d6705fd2efa91a
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-03 10:46:35 +0700

    new job form

--- src/screens/NewJobScreen.js
+++ src/screens/NewJobScreen.js
@@ -1,12 +1,84 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, ScrollView } from "react-native"
+import { Switch } from "react-native"
+import { FormLabel, FormInput, Button } from "react-native-elements"
 
 class NewJobScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: "Post a New Job",
+  }
+
+  state = {
+    showSalary: false,
+    job: {}
+  }
+
+  handleChange = (value, field) => {
+    this.setState({
+      job: {
+        ...this.state.job,
+        [field]: value,
+      }
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>NewJobScreen</Text>
-      </View>
+      <ScrollView>
+        <FormLabel>Job name</FormLabel>
+        <FormInput onChangeText={val => this.handleChange(val, "name")} />
+
+        <FormLabel>Company name</FormLabel>
+        <FormInput onChangeText={val => this.handleChange(val, "company")} />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "flex-start" }}>
+          <FormLabel>Show salary</FormLabel>
+          <Switch
+            onValueChange={(val) => { this.setState({ showSalary: val }) }}
+            value={this.state.showSalary}
+            style={{ marginRight: 15, marginTop: 15 }}
+          />
+        </View>
+
+        {
+          this.state.showSalary && (
+            <View style={{ backfaceVisibility: "hidden", flexDirection: "row" }}>
+              <View style={{ width: "50%" }}>
+                <FormLabel>From</FormLabel>
+                <FormInput
+                  keyboardType="numeric"
+                  onChangeText={val => this.handleChange(val, "salary_from")}
+                />
+              </View>
+
+              <View style={{ width: "50%" }}>
+                <FormLabel>To</FormLabel>
+                <FormInput
+                  keyboardType="numeric"
+                  onChangeText={val => this.handleChange(val, "salary_to")}
+                />
+              </View>
+            </View>
+          )
+        }
+
+        <FormLabel>Location</FormLabel>
+        <FormInput onChangeText={val => this.handleChange(val, "location")} />
+
+        <FormLabel>Job description</FormLabel>
+        <FormInput
+          multiline
+          numberOfLines={3}
+          onChangeText={val => this.handleChange(val, "description")}
+        />
+
+        <View style={{ marginTop: 10, marginBottom: 10 }}>
+          <Button
+            title="Save"
+            onPress={() => { }}
+          />
+        </View>
+      </ScrollView>
     )
   }
 }
