faeba83fe667ec4348c20216459e08c7e633992f
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-09 13:37:50 +0700

    Android: status bar

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from "react"
 import { Provider } from "react-redux"
+import { View, Platform } from "react-native"
+import { Constants } from "expo"
 
 import Routes from "./src/Routes"
 
 import store from "./src/store"
 
+const paddingTop = Platform.OS === "ios" ? 0 : Constants.statusBarHeight
+
+/* eslint no-console: 0 */
+console.ignoredYellowBox = [
+  'Setting a timer',
+]
+
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <View style={{ flex: 1, paddingTop }}>
+      <Routes />
+    </View>
   </Provider>
 )
 
